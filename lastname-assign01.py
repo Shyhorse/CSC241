3f551e1b84c8d69d4a7124a2e476c769e6b81151
Shyhorse <156483021+Shyhorse@users.noreply.github.com>
2024-01-12 17:35:03 -0600

    Add files via upload

---
+++ lastname-assign01.py
@@ -0,0 +1,27 @@
+#problem 1
+s1 ='&'
+s2 = '#'
+
+#your code here
+ 
+
+#spacer
+print()
+print()
+
+#problem 2
+
+first = "" #your code here
+last = "" #your code here
+print("") #your code here
+
+
+#spacer
+print()
+print()
+
+#problem 3
+
+bill = "" #your code here
+percent = "" #your code here
+#your code here
